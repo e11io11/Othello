0ed9d1aab043951b0942b5be866dfd8dde500d68
e11io11 <elliottfalguerolle@gmail.com>
2019-01-15 19:42:29 +0100

    sddsq

--- test_partie3.py
+++ test_partie3.py
@@ -41,20 +41,3 @@ p = creer_partie(4)
 print(saisie_valide(p, "M") == True)
 print(saisie_valide(p, "b1") == True)
 print(saisie_valide(p, "b4") == False)
-print("")
-
-
-
-#Test tour_jeu:
-"""
-print("tour_jeu:")
-p = creer_partie(4)
-tour_jeu(p)
-#Si l'utilisateur a saisi b1, alors p vaut :
-print(p=={ "joueur" : 1,
-      "plateau" : {
-        "n" : 4,
-        "cases" : [0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 2, 0, 0, 0, 0, 0]
-      }
-    })
-"""
